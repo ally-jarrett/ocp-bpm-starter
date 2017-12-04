f5ea59299d6bf5e970aca5d6c5ed21c423bee5f6
Ally Jarrett <ajarrett@redhat.com>
2017-12-04 18:08:53 +0000

    Initial

---
+++ openshift-test-deployment/src/test/java/com/ocp/hello_wlrd/ExampleTests.java
@@ -0,0 +1,37 @@
+package com.ocp.hello_wlrd;
+
+import org.junit.Test;
+
+public class ExampleTests {
+  
+  @Test
+  public void exampleTest1(){
+    System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> EXECUTING TEST 1....");
+  }
+
+  @Test
+  public void exampleTest2(){
+    System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> EXECUTING TEST 2....");
+  }
+
+  @Test
+  public void exampleTest3(){
+    System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> EXECUTING TEST 3....");
+  }
+
+  @Test
+  public void exampleTest4(){
+    System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> EXECUTING TEST 4....");
+  }
+
+  @Test
+  public void exampleTest5(){
+    System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> EXECUTING TEST 5....");
+  }
+
+  @Test
+  public void exampleTest6(){
+    System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> EXECUTING TEST 6....");
+  }
+  
+}
